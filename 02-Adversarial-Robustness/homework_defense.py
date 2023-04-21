71429e152489363ed35a57468d1e9134bda9431c
DylanWRh <93636459+DylanWRh@users.noreply.github.com>
2023-04-22 03:45:12 +0800

    Add files via upload

--- 02-Adversarial-Robustness/homework_defense.py
+++ 02-Adversarial-Robustness/homework_defense.py
@@ -6,11 +6,14 @@ from torch.autograd import Variable
 import torch.optim as optim
 from torchvision import transforms
 from models.resnet import *
+from models import wideresnet
+import torch.nn as nn
+import torch.nn.functional as F
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR TRADES Adversarial Training')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
                     help='input batch size for testing (default: 128)')
 parser.add_argument('--epochs', type=int, default=2, metavar='N',
                     help='number of epochs to train')
@@ -93,7 +96,44 @@ def PGD(model,
     return loss
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def trades_loss(model,
+                   x_natural,
+                   y,
+                   optimizer,
+                   step_size=0.003,
+                   epsilon=0.031,
+                   perturb_steps=10,
+                   beta=1.0,
+                   device=torch.device("cuda")):
+    criterion_kl = nn.KLDivLoss(size_average=False)
+    model.eval()
+    batch_size = len(x_natural)
+    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
+    for _ in range(perturb_steps):
+        x_adv.requires_grad_()
+        with torch.enable_grad():
+            loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
+                                       F.softmax(model(x_natural), dim=1))
+        grad = torch.autograd.grad(loss_kl, [x_adv])[0]
+        x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
+        x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
+        x_adv = torch.clamp(x_adv, 0.0, 1.0)
+
+    model.train()
+
+    x_adv = Variable(torch.clamp(x_adv, 0.0, 1.0), requires_grad=False)
+    # zero gradient
+    optimizer.zero_grad()
+    # calculate loss
+    logits = model(x_natural)
+    loss_natural = F.cross_entropy(logits, y)
+    loss_robust = (1.0 / batch_size) * criterion_kl(F.log_softmax(model(x_adv), dim=1),
+                                                    F.softmax(model(x_natural), dim=1))
+    loss = loss_natural + beta * loss_robust
+    return loss
+
+
+def train(args, model, device, train_loader, optimizer, epoch, train_nat=False, beta=6.0):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -101,14 +141,29 @@ def train(args, model, device, train_loader, optimizer, epoch):
         optimizer.zero_grad()
 
         # calculate robust loss
-        loss = PGD(model=model,
-                           x_natural=data,
-                           y=target,
-                           optimizer=optimizer,
-                           step_size=args.step_size,
-                           epsilon=args.epsilon,
-                           perturb_steps=args.num_steps,
-                           device = device)
+        ######################################################
+        # Change loss computing algorithm
+        if not train_nat:
+            loss = PGD(model=model,
+                              x_natural=data,
+                              y=target,
+                              optimizer=optimizer,
+                              step_size=args.step_size,
+                              epsilon=args.epsilon,
+                              perturb_steps=args.num_steps,
+                              device = device)
+            '''loss = trades_loss(model=model,
+                               x_natural=data,
+                               y=target,
+                               optimizer=optimizer,
+                               step_size=args.step_size,
+                               epsilon=args.epsilon,
+                               perturb_steps=args.num_steps,
+                               beta=6.0,
+                               device = device)'''
+        else:
+            loss = F.cross_entropy(model(data), target)
+        ######################################################
         loss.backward()
         optimizer.step()
 
@@ -174,8 +229,12 @@ def main():
     # init model, ResNet18() can be also used here for training
     import time
     start_time = time.time()
-    model = ResNet18().to(device)
-
+    
+    ######################################################
+    # Change model
+    # model = ResNet18().to(device)
+    model = wideresnet.WideResNet().to(device)
+    ######################################################
 
     def count_parameters(model, only_trainable=False):
         if only_trainable:
@@ -195,7 +254,11 @@ def main():
         adjust_learning_rate(optimizer, epoch)
 
         # adversarial training
-        train(args, model, device, train_loader, optimizer, epoch)
+        ######################################################
+        # Change training strategy
+        # train(args, model, device, train_loader, optimizer, epoch)
+        train(args, model, device, train_loader, optimizer, epoch, train_nat=(epoch==1))
+        ######################################################
 
         # evaluation on natural examples
         print('================================================================')
